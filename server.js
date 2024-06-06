507b78e681f3ad921c5d60f78e48887b38ed574a
Soulaiman El Maimouni Mechbal <soulaiman.elmaimouni@nttdata.com>
2024-06-06 05:06:17 +0200

    cors

--- server.js
+++ server.js
@@ -1,56 +1,91 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
-let http = require('http').Server(app);
-let io = require('socket.io')(http);
-
-
-
-const port = process.env.PORT || 8080;
-
-// 
-app.use(express.static(__dirname + '/public'));
-
-http.listen(port, () => {
-    console.log('listening on *:' + port);
+app.use(cors());
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+  cors: { origin: 'http://localhost:3001' }
 });
+const PORT = process.env.PORT || 8080;
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+  console.log('Client connected:', socket.id);
 
-    socket.on('join-room', (room) => {
-        console.log('create or join to room: ' + room);
-        console.log('socket id: ' + socket.id);
+  socket.on('message', (message) => {
+    console.log('Received message type:', message.type);
 
-        const myRoom = io.sockets.adapter.rooms[room] || { length: 0 };
-        console.log("My room: ",myRoom);
-        const numClients = myRoom.length;
-        console.log(room + ' has ' + numClients + ' clients');
+    const messageType = message.type;
+
+    switch (messageType) {
+      case 'ready':
+        const roomName = message.room;
+        const room = io.sockets.adapter.rooms.get(roomName) || new Set();
+        const numClients = room.size;
+
+        console.log(`Users in the room (${roomName}):`, numClients);
+        console.log("Room", room);
+        console.log("Message Room", roomName);
 
         if (numClients === 0) {
-            socket.join(room);
-            socket.emit('created', room);
+          socket.join(roomName);
+          message.type = "create-room";
+          socket.broadcast.to(roomName).emit('message', message);
+          console.log("Room created");
+
+          const updatedRoom = io.sockets.adapter.rooms.get(roomName);
+          console.log("Room", updatedRoom);
+          console.log("Number of clients", updatedRoom.size);
         } else if (numClients === 1) {
-            socket.join(room);
-            socket.emit('joined', room);
-        } else {
-            socket.emit('full', room);
+          socket.join(roomName);
+          message.type = "join-room";
+          socket.broadcast.to(roomName).emit('message', message);
+          console.log("Room joined");
+
+          const updatedRoom = io.sockets.adapter.rooms.get(roomName);
+          console.log("Room", updatedRoom);
+          console.log("Number of clients", updatedRoom.size);
+        } else if (numClients >= 2) {
+          console.log("Room full");
+          const updatedRoom = io.sockets.adapter.rooms.get(roomName);
+          console.log("Room", updatedRoom);
+          console.log("Number of clients", updatedRoom.size);
         }
-    })
+        break;
+      case "offer":
+      case "answer":
+      case "candidate":
+        console.log("Message type forwarded:", message.type);
+        console.log("Room:", message.room);
+        if (socket.rooms.has(message.room)) {
+          socket.to(message.room).emit('message', message);
+        }
+        break;
+      case "bye":
+        console.log("Handling bye for room:", message.room);
+        if (socket.rooms.has(message.room)) {
+          socket.to(message.room).emit('message', message);
+          socket.leave(message.room);
+        }
+        break;
+      default:
+        console.log('Unhandled message type:', messageType);
+        break;
+    }
+  });
 
+  socket.on('disconnect', () => {
+    console.log("Client disconnected:", socket.id);
+  });
+});
 
-    socket.on('ready', (appointment) => {
-        socket.broadcast.to(appointment).emit('ready');
-    });
+function error(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
+}
 
-    socket.on('candidate', (event) => {
-        socket.broadcast.to(event.room).emit('candidate', event.candidate);
-    });
+app.use(error);
 
-    socket.on('offer', (event) => {
-        socket.broadcast.to(event.room).emit('offer', event.sdp);
-    });
-
-    socket.on('answer', (event) => {
-        socket.broadcast.to(event.room).emit('answer', event.sdp);
-    });
+server.listen(PORT, () => {
+  console.log('Listening on port', PORT);
 });
